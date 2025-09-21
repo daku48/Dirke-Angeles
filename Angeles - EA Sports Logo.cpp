dd4fbe32ec271e916e5202812d87960026a355dd
dirke <angelesdirke@gmail.com>
2025-09-22 01:49:18 +0800

    Initial commit

---
+++ Angeles - EA Sports Logo.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	const char*logo = R"(
+
+
+                          XXXXXXXXXXXXX                             
+                     XXXXXXXXXXXXXXXXXXXXXXX                        
+                  XXXXXXXXXXXXXXXXXXXXXXXXXXXXX                     
+                XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX                   
+              XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX                 
+             XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX                
+            XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX               
+           XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX              
+          XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX             
+         XXXXXXXXXX            XXXXXXX    XXXXXXXXXXXXXX            
+         XXXXXXXX            XXXXXXXX      XXXXXXXXXXXXX            
+         XXXXXXXXXXXXXXXXXXXXXXXXXXX   X    XXXXXXXXXXXX            
+         XXXXXX               XXXXX   XXX    XXXXXXXXXXX            
+         XXXX                XXXXX   XXXXX    XXXXXXXXXX            
+          XXXXXXX   XXXXXXXXXXXXX              XXXXXXXX             
+           XXXXX   XXXXXXXXXXXXX   XXXXXXXXX    XXXXXX              
+            XXX                   XXXXXXXXXXX    XXXX               
+             XX                  XXXXXXXXXXXXX    XX                
+              XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX                 
+                XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX                   
+                  XXXXXXXXXXXXXXXXXXXXXXXXXXXXX                     
+                     XXXXXXXXXXXXXXXXXXXXXXX                        
+                          XXXXXXXXXXXXX                             
+       
+       )";
+
+	cout << logo <<"\n";
+	return 0;
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# Dirke-Angeles" 
